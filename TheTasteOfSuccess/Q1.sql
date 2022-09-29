b286bf006796cd3d41cbf4df11fe5217dbd4899a
rohitajay <90746660+rohitajay@users.noreply.github.com>
2022-09-29 14:14:46 +0530

    Update Q1.sql

--- TheTasteOfSuccess/Q1.sql
+++ TheTasteOfSuccess/Q1.sql
@@ -1,4 +1,4 @@
-Q1. What is the total amount each customer spent at the restaurant?
+-- Q1. What is the total amount each customer spent at the restaurant?
 
 SELECT s.customer_id, SUM(menu.price) AS total_sales
 FROM sales s 
@@ -6,3 +6,11 @@ INNER JOIN menu
 ON s.product_id = menu.product_id
 GROUP BY s.customer_id
 ;
+
+
+
+-- How many days has each customer visited the restaurant?
+
+SELECT sales.customer_id, COUNT(DISTINCT(sales.order_date)) AS total_days
+FROM sales
+GROUP BY sales.customer_id;
